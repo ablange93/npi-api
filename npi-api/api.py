dbda7ffd2e4a2e2e457487b2a41c7e22da750dab
Andrew Lange <andrew.lange93@gmail.com>
2020-03-08 14:52:11 -0400

    api/provider endpoint

--- npi-api/api.py
+++ npi-api/api.py
@@ -9,6 +9,7 @@ import json
 #######################################################################################
 NPI_DB_FILE = 'data/npi.db'
 NPI_TABLE_NAME = 'tblNpi'
+ENDPOINT_TABLE_NAME = 'tblEndpoint'
 NPI_COLUMN_NAME = 'NPI'
 
 
@@ -19,7 +20,7 @@ app.config["DEBUG"] = True
 #######################################################################################
 # HELPER FUNCTIONS #
 #######################################################################################
-def query_npi_id(c, npi_id):
+def query_npi(c, npi_id):
     # you could use 'one' param from stackoverflow to return only 1x result (use this when joining many results)
     c.execute('SELECT * FROM {table} WHERE {column}={npi_id}'.
               format(table=NPI_TABLE_NAME, column=NPI_COLUMN_NAME, npi_id=npi_id))
@@ -29,21 +30,29 @@ def query_npi_id(c, npi_id):
     return json_output
 
 
+def query_endpoint(c, npi_id):
+    c.execute('SELECT * FROM {table} WHERE {column}={npi_id}'.
+              format(table=ENDPOINT_TABLE_NAME, column=NPI_COLUMN_NAME, npi_id=npi_id))
+    query_result = [dict((c.description[i][0], value)
+              for i, value in enumerate(row)) for row in c.fetchall()]
+    json_output = json.loads(str(query_result[0]).replace("\'", "\""))
+    return json_output
+
 #######################################################################################
 # API ENDPOINTS #
 #######################################################################################
-# @app.route('/', methods=['GET'])
-# def home():
-#     return "<h1>NPI Data Web Interface</h1><p>This site is a prototype API that enables users to access NPI data.</p>"
+@app.route('/npi-api/v1.0/', methods=['GET'])
+def home():
+    return "<h1>NPI Data Web Interface</h1><p>This site is a prototype API that enables users to access NPI data.</p>"
 
 
-@app.route('/api/v1.0/provider', methods=['GET'])
-def query_example():
+@app.route('/npi-api/v1.0/provider', methods=['GET'])
+def provider():
     conn = sqlite3.connect(NPI_DB_FILE)
     cur = conn.cursor()
 
     npi_to_query = request.args.get('npiId')
-    json_response = query_npi_id(cur, npi_to_query)
+    json_response = query_npi(cur, npi_to_query)
 
     npi = json_response['NPI']
     type_code = json_response['EntityTypeCode']
@@ -59,6 +68,18 @@ def query_example():
                                                                              provider_business_name,
                                                                              provider_address,
                                                                              provider_state)
+
+
+@app.route('/npi-api/v1.0/endpoint', methods=['GET'])
+def endpoint():
+    conn = sqlite3.connect(NPI_DB_FILE)
+    cur = conn.cursor()
+
+    npi_to_query = request.args.get('npiId')
+    json_response = query_endpoint(cur, npi_to_query)
+
+    npi = json_response['NPI']
+    return '''<h3>NPI: {}</h3>'''.format(npi)
 
 
 #######################################################################################
